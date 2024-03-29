9809b98b0764484416dfe655613f6798803b76fb
Linux Rustling <109832568+TuxMC-sys@users.noreply.github.com>
2024-03-29 11:45:39 -0400

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,6 +1,20 @@
-fn main(){
-    let c =  |b, c| b + c;
-    let a =  |d: &str, e: &str| d.to_owned() + e;
-    println!("{}",c(1,2));
-    println!("{}",a("test"," test2"));
+fn main() {
+    let func_ops: (u64, i32, [i32;3]) = (20,70, [7,69,420]);
+    let s = "Hello,";
+    let new_s = append_world(&s);
+    println!("{}", new_s);
+    println!("Fibonacci number {} is {}.",func_ops.0, fib(func_ops.0));
+    println!("The sum of {:?} is {}.", func_ops.2, sum(&func_ops.2));
+    println!("{} degrees fahrenheit is {} degrees celcius.", func_ops.1, ftc(func_ops.1));
+}
+fn sum(nums: &[i32]) -> i32 {nums.iter().sum()}
+fn fib(n: u64) -> u64{
+    match n {
+        0 | 1 => 1,
+        _ => fib(n - 1) + fib(n - 2),
+    }
+}
+fn ftc(n: i32) -> i32{((f64::from(n)-32.0)*(5.0/9.0)).round() as i32}
+fn append_world(s: &str)->String{
+    String::from(s) + " world!"
 }
