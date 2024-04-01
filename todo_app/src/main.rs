da8baeffb7fadcbd8bb978a4288d14ae99ff260d
TuxMC-sys <109832568+TuxMC-sys@users.noreply.github.com>
2024-04-01 13:44:37 -0400

    added option to simply list todo list

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,4 +1,3 @@
-
 use std::{fs,str,io, io::Write};
 use serde::{Deserialize, Serialize};
 use serde_json::to_writer;
@@ -14,23 +13,39 @@ struct Todo {
 fn main(){
     println!("This program reads/writes from/to ~/Documents/rust_todo.json\n");
     let mut todos = parse_json().todo_list;
-    remove_todos(&mut todos);
-    add_todos(&mut todos);
-    save_todo_list(&todos);
+    if let ask_user_continue(){}
+        remove_todos(&mut todos);
+        add_todos(&mut todos);
+        save_todo_list(&todos);
+    }
 }
 
-fn save_todo_list(todos: &Vec<String>){
-    let mut save_string: String = String::from("");
-    println!("Your todos are \n {:#?}.", todos);
-    print_flush("Do you want to save the todo list (y, yes, Y or n, no, N)?: ");
-    io::stdin().read_line(&mut save_string).expect("Failed to read line"); 
+fn ask_user_continue() -> bool{
+    let mut user_response = "";
+    println!("If you want to edit your todo list, type y or yes then hit enter, else just hit enter.");
+    io::stdin().read_line(&mut user_response).expect("Failed to read line"); 
+    for val in YES_LIST{if user_response == *val{
+        true
+    }
+    false
+}
 
-    for val in YES_LIST{if save_string == *val{
-        let save_string = Todo{
-            todo_list: todos.to_owned()
-        };
-        to_writer(fs::File::create_new(TODO_FILE).unwrap(),&save_string).unwrap();
-    }}
+fn print_current_todo(todos: &[String]){
+    println!("Your current todo's are:");
+    todos.iter().enumerate().for_each(|(i,x)|println!("{}. {}",i+1,x));
+}
+
+fn print_flush(print_str: &str){
+    print!("{}",print_str); 
+    io::stdout().flush().unwrap();
+}
+
+fn parse_json() -> Todo{
+    serde_json::from_str(&init_file()).unwrap()
+}
+
+fn init_file() -> String{
+    str::from_utf8(&fs::read(TODO_FILE).expect("File not found")).unwrap().to_string()
 }
 
 fn add_todos(todos: &mut Vec<String>){
@@ -63,20 +78,17 @@ fn remove_todos(todos: &mut Vec<String>){
     }
 }
 
-fn print_current_todo(todos: &[String]){
-    println!("Your current todo's are:");
-    todos.iter().enumerate().for_each(|(i,x)|println!("{}. {}",i+1,x));
+fn save_todo_list(todos: &Vec<String>){
+    let mut save_string: String = String::from("");
+    println!("Your todos are \n {:#?}.", todos);
+    print_flush("Do you want to save the todo list (y, yes, Y or n, no, N)?: ");
+    io::stdin().read_line(&mut save_string).expect("Failed to read line"); 
+
+    for val in YES_LIST{if save_string == *val{
+        let save_string = Todo{
+            todo_list: todos.to_owned()
+        };
+        to_writer(fs::File::create_new(TODO_FILE).unwrap(),&save_string).unwrap();
+    }}
 }
 
-fn parse_json() -> Todo{
-    serde_json::from_str(&init_file()).unwrap()
-}
-
-fn init_file() -> String{
-    str::from_utf8(&fs::read(TODO_FILE).expect("File not found")).unwrap().to_string()
-}
-
-fn print_flush(print_str: &str){
-    print!("{}",print_str); 
-    io::stdout().flush().unwrap();
-}
