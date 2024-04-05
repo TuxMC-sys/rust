d8dc74bdc68eee5625814b0fde5ed16b3bd73b70
TuxMC-sys <109832568+TuxMC-sys@users.noreply.github.com>
2024-04-05 10:57:00 -0400

    Up to end of Listing 12-10

--- bookgrep/src/main.rs
+++ bookgrep/src/main.rs
@@ -1,19 +1,34 @@
-use std::env;
-use std::fs;
+use std::{env, fs, process};
+
+struct Input {
+    search_string: String,
+    rel_path: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let (search_string, rel_path) = parse_env(&args);
-    
-    let contents = fs::read_to_string(rel_path)
-        .expect("Could not read file.");
-    
-    println!("Searching for {} in {}", search_string, rel_path);
+    let config = Input::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!(
+        "Searching for {} in {}",
+        config.search_string, config.rel_path
+    );
+    let contents = fs::read_to_string(config.rel_path).expect("Could not read file.");
     println!("The file contains:\n{}", contents)
 }
-fn parse_env(args: &[String]) -> (&str, &str){
-    let search_string = &args[1];
-    let rel_path = &args[2];
 
-    (search_string, rel_path)
+impl Input {
+    fn build(args: &[String]) -> Result<Input, &'static str> {
+        if args.len() < 3 {
+            return Err("You must specify the search string and the file to search in.");
+        }
+        let search_string = args[1].clone();
+        let rel_path = args[2].clone();
+        Ok(Input {
+            search_string,
+            rel_path,
+        })
+    }
 }
