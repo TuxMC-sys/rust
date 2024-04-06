6b9d7377b2078efdd31c150e7feba1f9e375af54
TuxMC-sys <109832568+TuxMC-sys@users.noreply.github.com>
2024-04-05 22:44:32 -0400

    publishing(fixed)

--- bookgrep/src/main.rs
+++ bookgrep/src/main.rs
@@ -1,32 +1,3 @@
-//!#Chapter 12 guided grep project
-//!If you have something better to do with this crate name, open a GitHub issue, I am pretty prompt.
-//!I feel like my code is pretty easy to understand but if it isn't, please reference chapter 12 of
-//!the book, I only changed some organization things and var names.
-//!
-//! Copyright (c) 2010 The Rust Project Developers
-//!
-//! Permission is hereby granted, free of charge, to any
-//! person obtaining a copy of this software and associated
-//! documentation files (the "Software"), to deal in the
-//! Software without restriction, including without
-//! limitation the rights to use, copy, modify, merge,
-//! publish, distribute, sublicense, and/or sell copies of
-//! is furnished to do so, subject to the following
-//! conditions:
-//!
-//! The above copyright notice and this permission notice
-//! shall be included in all copies or substantial portions
-//! of the Software.
-//!
-//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
-//! ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-//! TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-//! PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-//! SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-//! CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-//! OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-//! IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//! DEALINGS IN THE SOFTWARE.
 use bookgrep::{run, Input};
 use std::{env, process};
 
